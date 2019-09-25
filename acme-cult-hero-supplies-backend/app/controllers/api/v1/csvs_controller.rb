bbb31a00d93b24cf0d038a376c8f1b266377fb56
Marcus <marcusgnf@gmail.com>
2019-09-24 21:44:46 -0500

    seeded database, checked routes

--- acme-cult-hero-supplies-backend/app/controllers/api/v1/csvs_controller.rb
+++ acme-cult-hero-supplies-backend/app/controllers/api/v1/csvs_controller.rb
@@ -1,4 +1,19 @@
 require 'csv'
 
 class Api::V1::CsvsController < ApplicationController
+    def create
+        csv_text = params["file"].tempfile
+        CSV.foreach(csv_text).with_index do |row, i|
+            next if i == 0
+            Sale.create(customer_name: row[0], item_description: row[1], item_price: row[2], quantity: row[3], 
+                merchant_name: row[4], merchant_address: row[5])
+          end
+    end
+    
+    def show
+    
+    end
+
+    private
+   
 end
--- acme-cult-hero-supplies-backend/app/controllers/api/v1/sales_controller.rb
+++ acme-cult-hero-supplies-backend/app/controllers/api/v1/sales_controller.rb
@@ -1,2 +1,5 @@
 class Api::V1::SalesController < ApplicationController
+    def index
+        render json: Sale.all
+    end
 end
